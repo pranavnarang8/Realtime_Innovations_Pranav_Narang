7f1e1dd9dd5fe78f795358a2deb09c0eaf367fbf
Pranav Narang <pranavnarang1997@gmail.com>
2023-10-03 13:34:25 +0530

    Redux Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import dateReducer from '../features/dateSlice';
 import roleReducer from '../features/roleSlice';
-import employeeReducer from "../features/employeeSlice"
+import employeeReducer from "../features/employeeSlice";
 
 export const store = configureStore({
   reducer: {
@@ -9,4 +9,12 @@ export const store = configureStore({
     role: roleReducer,
     employee: employeeReducer,
   },
+
+  middleware:(getDefaultMiddleware) =>{
+    return getDefaultMiddleware({
+     serializableCheck:{
+        ignoredActions:["employee/setEmployee", "role/resetProfile", "role/chooseRole"]
+      }
+    })
+  }
 });
